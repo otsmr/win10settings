c0867221d0df3ef0d45db5c758f5acd321831017
Tobias Müller <code@tsmr.eu>
2020-11-05 19:26:23 +0100

    fix #6: update bsi link (#7)

--- src/components/settings/tableofcontent.tsx
+++ src/components/settings/tableofcontent.tsx
@@ -6,7 +6,7 @@ import A from "./forms/a"
 import search from "../../utilitis/search"
 import socket from '../../utilitis/socket';
 
-const BSI_BERICHT_LINK = "https://www.bsi.bund.de/SharedDocs/Downloads/DE/BSI/Cyber-Sicherheit/SiSyPHus/Analyse_Telemetriekomponente.pdf?__blob=publicationFile&v=5";
+const BSI_BERICHT_LINK = "https://www.bsi.bund.de/SharedDocs/Downloads/DE/BSI/Cyber-Sicherheit/SiSyPHus/Analyse_Telemetriekomponente_1_2.pdf?__blob=publicationFile&amp;v=6";
 
 function BSIInfo () {
     return (
