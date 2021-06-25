55a0c5a81136429d1e3363787d7ba5440d129a00
Tobias Müller <git@tsmr.eu>
2021-06-25 23:22:50 +0200

    add settings

--- backend/src/webview.rs
+++ backend/src/webview.rs
@@ -4,9 +4,9 @@ pub fn start_webview(nonce: String) {
 
     let mut webview = WebviewBuilder::new()
         .debug(true)
-        .title("win10settings")
-        .width(1800)
-        .height(1400)
+        .title("Erweitere Windows-Einstellungen")
+        .width(1600)
+        .height(1000)
         .resize(SizeHint::NONE)
         .url(&(String::from("http://127.0.0.1:3002/index.html#nonce=") + &nonce))
         .build();
